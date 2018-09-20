f1c3a0eb3a4bc04b344ab86c87d443a82871c98a
Brandon <thegtproject@live.com>
2018-09-19 22:34:34 -0600

    support new attributes and uniforms types. minor refactoring

--- internal/gravitygl/opengl/program.go
+++ internal/gravitygl/opengl/program.go
@@ -1,101 +1,122 @@
 package opengl
 
 import (
+	"fmt"
+
 	"github.com/go-gl/gl/v4.3-core/gl"
 )
 
 // Program ...
 type Program struct {
-	loc      ProgramLoc
+	ID       ProgramLoc
 	Compiled bool
-	attr     *Attributes
-	unif     *Uniforms
+	Attr     *Attributes
+	Unif     *Uniforms
+}
+
+func (prg *Program) String() string {
+	return fmt.Sprintf(
+		`Program
+ ID:      %d
+ Compiled: %v
+ Attributes: %v
+ Uniforms:   %v
+ `,
+		prg.ID, prg.Compiled, prg.Attr.data, prg.Unif.data,
+	)
 }
 
 // NewProgram ...
 func NewProgram(vertexShaderSrc, fragmentShaderSrc string) *Program {
-	program := &Program{
-		loc:  gl.CreateProgram(),
-		attr: NewAttributesMap(),
-		unif: NewUniformsMap(),
+	prg := &Program{
+		ID:   gl.CreateProgram(),
+		Attr: NewAttributesMap(),
+		Unif: NewUniformsMap(),
 	}
 
 	vertexShader := NewVertexShader(vertexShaderSrc)
 	fragmentShader := NewFragmentShader(fragmentShaderSrc)
 
-	gl.AttachShader(program.loc, vertexShader.loc)
-	gl.AttachShader(program.loc, fragmentShader.loc)
-	gl.LinkProgram(program.loc)
+	gl.AttachShader(prg.ID, vertexShader.loc)
+	gl.AttachShader(prg.ID, fragmentShader.loc)
+	gl.LinkProgram(prg.ID)
 
 	var success int32
-	gl.GetProgramiv(program.loc, LINK_STATUS, &success)
+	gl.GetProgramiv(prg.ID, LINK_STATUS, &success)
 	if success == FALSE {
 		var logLen int32
-		gl.GetProgramiv(program.loc, INFO_LOG_LENGTH, &logLen)
+		gl.GetProgramiv(prg.ID, INFO_LOG_LENGTH, &logLen)
 		infoLog := make([]byte, logLen)
-		gl.GetProgramInfoLog(program.loc, logLen, nil, &infoLog[0])
+		gl.GetProgramInfoLog(prg.ID, logLen, nil, &infoLog[0])
 		panic("error linking shader program: " + string(infoLog))
 	}
 
 	{
-		count := program.GetProgramIV(ACTIVE_ATTRIBUTES)
+		count := prg.GetProgramIV(ACTIVE_ATTRIBUTES)
 		var idx uint32
 		for idx = 0; idx < uint32(count); idx++ {
-			name, size, ty := program.GetActiveAttrib(idx)
-			program.attr.Add(name, idx, ty, size)
+			name, size, ty := prg.GetActiveAttrib(idx)
+			prg.Attr.Add(name, idx, ty, size)
 		}
 	}
 
 	{
-		count := program.GetProgramIV(ACTIVE_UNIFORMS)
+		count := prg.GetProgramIV(ACTIVE_UNIFORMS)
 		var idx uint32
 		for idx = 0; idx < uint32(count); idx++ {
-			name, size, ty := program.GetActiveUniform(idx)
-			program.unif.Add(name, idx, ty, size)
+			name, size, ty := prg.GetActiveUniform(idx)
+			prg.Unif.Add(name, idx, ty, size)
 		}
 	}
 
-	program.Compiled = true
+	prg.Compiled = true
 	gl.DeleteShader(vertexShader.loc)
 	gl.DeleteShader(fragmentShader.loc)
-	return program
+
+	prg.Use()
+	return prg
 }
 
 // GetProgramIV ...
-func (p *Program) GetProgramIV(pname uint32) int32 {
+func (prg *Program) GetProgramIV(pname uint32) int32 {
 	var value int32
-	gl.GetProgramiv(p.loc, pname, &value)
+	gl.GetProgramiv(prg.ID, pname, &value)
 	return value
 }
 
 // GetActiveAttrib ...
-func (p *Program) GetActiveAttrib(index uint32) (name string, size int, ty Enum) {
+func (prg *Program) GetActiveAttrib(index uint32) (name string, size int, ty Enum) {
 	// ACTIVE_ATTRIBUTE_MAX_LENGTH returns the length of the largest attribute name
 	// so we can prepare a buffer large enough to contain all possible attribute names
-	nameBufferSize := p.GetProgramIV(ACTIVE_ATTRIBUTE_MAX_LENGTH)
+	nameBufferSize := prg.GetProgramIV(ACTIVE_ATTRIBUTE_MAX_LENGTH)
 	nameBuffer := make([]byte, nameBufferSize)
 
 	// _size   will return the size of the attribute, in units of the type returned in type.
 	// _length will be the length of the attribute's name
 	var _size, _length int32
 	var _type uint32
-	gl.GetActiveAttrib(p.loc, index, nameBufferSize, &_length, &_size, &_type, &nameBuffer[0])
+	gl.GetActiveAttrib(prg.ID, index, nameBufferSize, &_length, &_size, &_type, &nameBuffer[0])
 
 	return string(nameBuffer[0:_length]), int(_size), Enum(_type)
 }
 
 // GetActiveUniform ...
-func (p *Program) GetActiveUniform(index uint32) (name string, size int, ty Enum) {
+func (prg *Program) GetActiveUniform(index uint32) (name string, size int, ty Enum) {
 	// ACTIVE_UNIFORM_MAX_LENGTH returns the length of the largest uniform name
 	// so we can prepare a buffer large enough to contain all possible uniform names
-	nameBufferSize := p.GetProgramIV(ACTIVE_UNIFORM_MAX_LENGTH)
+	nameBufferSize := prg.GetProgramIV(ACTIVE_UNIFORM_MAX_LENGTH)
 	nameBuffer := make([]byte, nameBufferSize)
 
 	// _size   will return the size of the uniform, in units of the type returned in type.
 	// _length will be the length of the uniform's name
 	var _size, _length int32
 	var _type uint32
-	gl.GetActiveUniform(p.loc, index, nameBufferSize, &_length, &_size, &_type, &nameBuffer[0])
+	gl.GetActiveUniform(prg.ID, index, nameBufferSize, &_length, &_size, &_type, &nameBuffer[0])
 
 	return string(nameBuffer[0:_length]), int(_size), Enum(_type)
+}
+
+// Use ...
+func (prg *Program) Use() {
+	gl.UseProgram(prg.ID)
 }
